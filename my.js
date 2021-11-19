8cb64464b49ea512544058bb2494ee2c2a113287
Emtiaz310 <emtiazhossainrzs@gmail.com>
2021-11-19 15:54:53 +0600

    edit

--- my.js
+++ my.js
@@ -1,11 +1,7 @@
 function luduGame (min , max){
     return Math.floor(Math.random() * (max - min + 1 )) + min;
 };
+console.log(luduGame(1, 6));
 
 const students =["Emtiaz", "Hasan","Rahim", "Suma", "Muna","Alif","Zion", "Sunio", "Amelia","Ethan ", "Lucas ", "Emma ","Logan ", "Owen ", "Stella ","Sebastian ","Alexander" ] ;
-
-document.getElementById('alphabetically').innerHTML = students;
-
-students.sort();
-
-document.getElementById('alphabetically2').innerHTML = students ;
+console.log(students.sort());
